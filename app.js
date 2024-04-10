ec53457ba742fa77bca0cc88b17a2fa3bda3d557
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-11 01:40:11 +0530

    fix:cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://docter-app-client.vercel.app"],
+    origin: ["*"],
     methods: ["*"],
     allowedHeaders: ["*"],
     exposedHeaders: ["Authorization"],
