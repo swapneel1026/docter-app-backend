540b1175dca7cf191e06dfc29fc35e9733cea454
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-11 12:04:36 +0530

    remove cors options

--- app.js
+++ app.js
@@ -15,16 +15,7 @@ const port = process.env.PORT || 8000;
 dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["*"],
-    allowedHeaders: ["*"],
-    exposedHeaders: ["Authorization"],
-    credentials: true,
-    preflightContinue: false,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(verifyCookie("token"));
